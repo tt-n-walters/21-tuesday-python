d68ea905e396f7b325d55019f339ac91a37429a3
tt-n-walters <nico.walters@techtalents.es>
2021-11-16 18:27:08 +0100

    Removed debug print statements

--- mazes.py
+++ mazes.py
@@ -37,76 +37,43 @@ def generate():
     # right
     next_x = x + 1
     next_y = y
-    # print("next", next_x, next_y)
     if 0 <= next_x < 10 and 0 <= next_y < 10:
         right = maze[next_y][next_x]
-        # print(right)
         if all(right.values()): # Check if all walls are still True
-            # print("Not visited")
             neighbours["right"] = next_x, next_y
-        # else:
-            # print("visited")
-    # else:
-        # print("off edge")
+
 
     # up
     next_x = x
     next_y = y - 1
-    # print("next", next_x, next_y)
     if 0 <= next_x < 10 and 0 <= next_y < 10:
         up = maze[next_y][next_x]
-        # print(up)
         if all(up.values()): # Check if all walls are still True
-            # print("Not visited")
             neighbours["up"] = next_x, next_y
-        # else:
-            # print("visited")
-    # else:
-        # print("off edge")
 
     # left
     next_x = x - 1
     next_y = y
-    # print("next", next_x, next_y)
     if 0 <= next_x < 10 and 0 <= next_y < 10:
         left = maze[next_y][next_x]
-        # print(left)
         if all(left.values()): # Check if all walls are still True
-            # print("Not visited")
             neighbours["left"] = next_x, next_y
-        # else:
-            # print("visited")
-    # else:
-        # print("off edge")
         
     # down
     next_x = x
     next_y = y + 1
-    # print("next", next_x, next_y)
     if 0 <= next_x < 10 and 0 <= next_y < 10:
         down = maze[next_y][next_x]
-        # print(down)
         if all(down.values()): # Check if all walls are still True
-            # print("Not visited")
             neighbours["down"] = next_x, next_y
-        # else:
-            # print("visited")
-    # else:
-        # print("off edge")
     
-    # print(neighbours)
-    # print("choosing random")
 
     if neighbours:
         direction = random.choice(list(neighbours))
         opposite_direction = opposites[direction]
-        # print("Moving", direction)
-        # print("opposite", opposite_direction)
 
         next_x, next_y = neighbours[direction]
-        # print("next cell", next_x, next_y)
         
-        # print("removing walls.")
         # remove wall from current cell
         maze[y][x][direction] = False
         # remove opposite wall from next cell

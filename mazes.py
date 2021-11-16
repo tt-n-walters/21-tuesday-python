e329c300d125688311bedb4d0c76f4465a730bee
tt-n-walters <nico.walters@techtalents.es>
2021-11-16 18:55:10 +0100

    Maze dynamic dimensions refactor finished

--- mazes.py
+++ mazes.py
@@ -16,9 +16,9 @@ padding = 10
 
 
 maze = []
-for _ in range(10):
+for _ in range(rows):
     row = []
-    for _ in range(10):
+    for _ in range(columns):
         cell = {
             "up": True,
             "down": True,
@@ -28,8 +28,8 @@ for _ in range(10):
         row.append(cell)
     maze.append(row)
 
-x = 0
-y = 0
+x = columns // 2
+y = rows // 2
 
 width = columns * cell_width + padding * 2
 height = rows * cell_height + padding * 2
@@ -45,7 +45,7 @@ def generate():
     # right
     next_x = x + 1
     next_y = y
-    if 0 <= next_x < 10 and 0 <= next_y < 10:
+    if 0 <= next_x < columns and 0 <= next_y < rows:
         right = maze[next_y][next_x]
         if all(right.values()): # Check if all walls are still True
             neighbours["right"] = next_x, next_y
@@ -54,7 +54,7 @@ def generate():
     # up
     next_x = x
     next_y = y - 1
-    if 0 <= next_x < 10 and 0 <= next_y < 10:
+    if 0 <= next_x < columns and 0 <= next_y < rows:
         up = maze[next_y][next_x]
         if all(up.values()): # Check if all walls are still True
             neighbours["up"] = next_x, next_y
@@ -62,7 +62,7 @@ def generate():
     # left
     next_x = x - 1
     next_y = y
-    if 0 <= next_x < 10 and 0 <= next_y < 10:
+    if 0 <= next_x < columns and 0 <= next_y < rows:
         left = maze[next_y][next_x]
         if all(left.values()): # Check if all walls are still True
             neighbours["left"] = next_x, next_y
@@ -70,7 +70,7 @@ def generate():
     # down
     next_x = x
     next_y = y + 1
-    if 0 <= next_x < 10 and 0 <= next_y < 10:
+    if 0 <= next_x < columns and 0 <= next_y < rows:
         down = maze[next_y][next_x]
         if all(down.values()): # Check if all walls are still True
             neighbours["down"] = next_x, next_y
@@ -93,19 +93,19 @@ def generate():
 
 def create_shapes():
     shapes_list = arcade.ShapeElementList()
-    for row in range(10):
-        for column in range(10):
-            x = column * 40 + 10
-            y = 420 - 40 - row * 40 - 10
+    for row in range(rows):
+        for column in range(columns):
+            x = column * cell_width + padding
+            y = height - cell_height - row * cell_height - padding
             cell = maze[row][column]
             if cell["down"]:
-                shapes_list.append(arcade.create_line(x, y, x+40, y, arcade.color.GREEN))  # down
+                shapes_list.append(arcade.create_line(x, y, x+cell_width, y, arcade.color.GREEN))  # down
             if cell["up"]:
-                shapes_list.append(arcade.create_line(x, y+40, x+40, y+40, arcade.color.GREEN)) # top
+                shapes_list.append(arcade.create_line(x, y+cell_height, x+cell_width, y+cell_height, arcade.color.GREEN)) # top
             if cell["left"]:
-                shapes_list.append(arcade.create_line(x, y, x, y+40, arcade.color.GREEN)) # left
+                shapes_list.append(arcade.create_line(x, y, x, y+cell_height, arcade.color.GREEN)) # left
             if cell["right"]:
-                shapes_list.append(arcade.create_line(x+40, y, x+40, y+40, arcade.color.GREEN)) # right
+                shapes_list.append(arcade.create_line(x+cell_width, y, x+cell_width, y+cell_height, arcade.color.GREEN)) # right
     return shapes_list
 
 def draw(delta):

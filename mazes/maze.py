1a31d77aa2b4ab5468d24dc1727c289bb5b4edaf
tt-n-walters <nico.walters@techtalents.es>
2021-11-30 18:22:25 +0100

    Refactored generate method

--- mazes/maze.py
+++ mazes/maze.py
@@ -1,3 +1,18 @@
+import random
+
+
+opposites = {
+    "up": "down",
+    "down": "up",
+    "left": "right",
+    "right": "left"
+}
+directions = {    # direction: (x, y)
+    "up": (0, -1),
+    "down": (0, 1),
+    "left": (-1, 0),
+    "right": (1, 0)
+}
 
 
 class Maze:
@@ -17,6 +32,48 @@ class Maze:
                 }
                 row.append(cell)
             self.grid.append(row)
+            
+            self.stack = []
+            self.first = True
+            self.x = columns // 2
+            self.y = rows // 2
+
+
+    def generate(self):
+
+        if self.first or self.stack:
+            self.first = False
+
+            neighbours = {}
+
+            for direction, movement in directions.items():
+                next_x = self.x + movement[0]
+                next_y = self.y + movement[1]
+                if 0 <= next_x < self.columns and 0 <= next_y < self.rows:
+                    next = maze[next_y][next_x]
+                    if all(next.values()): # Check if all walls are still True
+                        neighbours[direction] = next_x, next_y
+
+
+            if neighbours:
+                direction = random.choice(list(neighbours))
+                opposite_direction = opposites[direction]
+
+                next_x, next_y = neighbours[direction]
+                # remove wall from current cell
+                maze[self.y][self.x][direction] = False
+                # remove opposite wall from next cell
+                maze[next_y][next_x][opposite_direction] = False
+                
+                self.stack.append((self.x, self.y))
+
+                self.x = next_x
+                self.y = next_y
+            
+            else:
+                # Dead end, no possible neighbours
+                self.x, self.y = self.stack.pop()
+
 
 
 maze = Maze(rows=5, columns=5)

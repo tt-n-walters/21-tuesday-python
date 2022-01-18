e83f8002b03e12f6544dfb44b3383bb9342eefff
tt-n-walters <nico.walters@techtalents.es>
2022-01-18 18:38:45 +0100

    Server User class

--- server/app.py
+++ server/app.py
@@ -1,9 +1,70 @@
 from flask import Flask, request
+import os
 
+
+folder_name = os.path.dirname(os.path.abspath(__file__)) + "/"
 app = Flask(__name__)
 
 
 logged_on = False
+
+
+class User:
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
+    
+    def __repr__(self):
+        return repr("User with name: " + repr(self.name))
+    
+    def check_name(self, name):
+        if name == self.name:
+            return True
+        else:
+            return False
+
+    def check_password(self, password):
+        if password == self.password:
+            return True
+        else:
+            return False
+
+    def check(self, name, password):
+        """
+            Checks username and password. Returns an int code depending.
+            0 -> Successful login
+            1 -> Incorrect password
+            2 -> Invalid username
+        """
+        if self.check_name(name):
+            if self.check_password(password):
+                return 0
+            else:
+                return 1
+        else:
+            return 2
+
+
+users = []
+
+def register(name, password):
+    for existing_user in users:
+        print("Checking", existing_user)
+        if existing_user.check_name(name):
+            print("User with name", name, "already exists.")
+            return
+
+    new_user = User(name, password)
+    users.append(new_user)
+    print("Registered new user", new_user)
+    print(users)
+
+
+register("alice", None)
+register("bob", "password")
+register("bob", "otherpassword")
+
+
 correct_user = "bob"
 correct_password = "techtalents"
 
@@ -22,17 +83,19 @@ def login():
         username = request.form.get("username")
         password = request.form.get("password")
         print("Login attempt with", repr(username), "and", repr(password))
-        if username == correct_user:
-            if password == correct_password:
+        for existing_user in users:
+            result = existing_user.check(username, password)
+            if result == 0:
                 logged_on = True
                 return "Logged in successfully."
-            else:
+            elif result == 1:
                 return "Password incorrect."
-        else:
-            return "User not found."
+            elif result == 2:
+                return "User not found."
+
             
     elif request.method == "GET":
-        with open("login.html", "r") as file:
+        with open(folder_name + "login.html", "r") as file:
             return file.read()
 
 
@@ -44,3 +107,7 @@ def logout():
         return "Successfully logged out."
     else:
         return "You are not logged in."
+
+
+
+app.run(debug=True, host="0.0.0.0", port=16000)
